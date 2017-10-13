7dc8cfdb81ec4fd7629bc5d86ff93bae29107740
Mauro <mauro_bilo@hotmail.com>
2017-10-13 20:23:20 -0300

    Actualizados al documento los valores de ubicacion y demanda.

--- IA - GeneticAlgorithm -TP/IA_TP.UI/Config.cs
+++ IA - GeneticAlgorithm -TP/IA_TP.UI/Config.cs
@@ -11,7 +11,7 @@ namespace IA_TP.UI
             TelcoSur telcoSur = new TelcoSur();
             telcoSur.Cities = BuildBaseCities();
             telcoSur.Catalogue = BuildBaseCatalogue();
-            telcoSur.FiberChannelKmsAvailable = 600;
+            telcoSur.FiberChannelKmsAvailable = 800;
             telcoSur.FiberChannelKmCost = 1000;
             telcoSur.PenaltyPercent = 50;
             return telcoSur;
@@ -31,29 +31,7 @@ namespace IA_TP.UI
         {
             //X <= 1000
             //Y <= 400
-            /*
-                Localidad A: 550 clientes potenciales
-                    125 Clientes requieres servicio de Cable
-                    75 Clientes Requieren servicio de Internet
-                    350 Clientes Requieren servicio de Telefonía
-                Localidad B: 732 clientes potenciales
-                    250 Clientes requieres servicio de Cable
-                    182 Clientes Requieren servicio de Internet
-                    300 Clientes Requieren servicio de Telefonía
-                Localidad C: 145 clientes potenciales
-                    100 Clientes requieres servicio de Cable
-                    25 Clientes Requieren servicio de Internet
-                    20 Clientes Requieren servicio de Telefonía
-                Localidad D: 133 clientes potenciales
-                    15 Clientes requieres servicio de Cable
-                    100 Clientes Requieren servicio de Internet
-                    18 Clientes Requieren servicio de Telefonía
-                Localidad E: 233 clientes potenciales
-                    73 Clientes requieres servicio de Cable
-                    130 Clientes Requieren servicio de Internet
-                    30 Clientes Requieren servicio de Telefonía
-
-             */
+           
             return new List<Model.City>
             {
                 new Model.City
@@ -64,48 +42,38 @@ namespace IA_TP.UI
                 new Model.City
                 {
                     Latitude = 250, Longitude = 300, Name = "Localidad B",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 },
+                    Demand = new UserDemand {Internet = 1000, Phone= 1600, TV = 2500 },
                 },
                 new Model.City
                 {
                     Latitude = 900, Longitude = 170, Name = "Localidad C",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
+                    Demand = new UserDemand {Internet = 400, Phone= 800, TV = 3500 }
                 },
                 new Model.City
                 {
                     Latitude = 790, Longitude = 300, Name = "Localidad D",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
+                    Demand = new UserDemand {Internet = 2000, Phone= 600, TV = 5500 }
                 },
                 new Model.City
                 {
                     Latitude = 750, Longitude = 30, Name = "Localidad E",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
+                    Demand = new UserDemand {Internet = 800, Phone= 200, TV = 1700 }
                 },
                 new Model.City
                 {
                     Latitude = 500, Longitude = 360, Name = "Localidad F",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
+                    Demand = new UserDemand {Internet = 2300, Phone= 1200, TV = 600 }
                 },
                 new Model.City
                 {
                     Latitude = 600, Longitude = 180, Name = "Localidad G",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
+                    Demand = new UserDemand {Internet = 500, Phone= 1900, TV = 1800 }
                 },
                 new Model.City
                 {
                     Latitude = 900, Longitude = 60, Name = "Localidad H",
                     Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
-                },
-                new Model.City
-                {
-                    Latitude = 500, Longitude = 360, Name = "Localidad I",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
-                },
-                new Model.City
-                {
-                    Latitude = 100, Longitude = 80, Name = "Localidad J",
-                    Demand = new UserDemand {Internet = 5000, Phone= 600, TV = 500 }
-                },
+                }
             };
         }
     }
